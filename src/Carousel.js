d0b1a709b7f1743d95a2860473399420ba24f9e7
Matthew Sia <msia075@uottawa.ca>
2022-07-02 18:52:52 -0400

    fix carousel wrap

--- src/Carousel.js
+++ src/Carousel.js
@@ -3,34 +3,53 @@ import rightArrow from './icons/right-arrow.jpg';
 import CourseCard from './CourseCard';
 
 function Carousel() {    
+    const shift = 115;
     function moveCarousel (event) {
-        const carousel = document.querySelector(".carousel");
-        let sliderIdx = parseInt(getComputedStyle(carousel).getPropertyValue("--slider-index"));
-
+        event.preventDefault();
         const clicked = event.target.closest(".carousel-button");
-
         const courseCards = [...document.querySelectorAll(".course-card")];
 
         if (clicked.matches(".carousel-button.left")) {
-            sliderIdx--;
-            if (sliderIdx <= -courseCards.length - 5) sliderIdx = 0;
             courseCards.forEach((card) => {
-                carousel.style.setProperty("--slider-index", sliderIdx);
-                card.style.transform = `translateX(${100 * sliderIdx}%)`;
+                const transform = card.style.getPropertyValue("transform");
+                let curr = transform.substr(11,).slice(0, -2);
+
+                if (isNaN(parseInt(curr))) {
+                    curr = 0;
+                } else {
+                    curr = parseInt(curr);
+                }
+                console.log(curr);
+                if (curr >= 6 * shift) {
+                    card.style.transform = `translateX(${-courseCards.length * shift + shift}%)`;
+                }
+                else {
+                    card.style.transform = `translateX(${curr + shift}%)`;
+                }
             })
         } else if (clicked.matches(".carousel-button.right")) {
-            sliderIdx++;
-            if (sliderIdx >= courseCards.length + 5) sliderIdx = 0;
             courseCards.forEach((card) => {
-                carousel.style.setProperty("--slider-index", sliderIdx);
-                card.style.transform = `translateX(${-100 * sliderIdx}%)`;
+                const transform = card.style.getPropertyValue("transform");
+                let curr = parseInt(transform.substr(11,).slice(0, -2));
+
+                if (isNaN(parseInt(curr))) {
+                    curr = 0;
+                } else {
+                    curr = parseInt(curr);
+                }
+                console.log(curr);
+                if (curr <= -courseCards.length * shift + shift) {
+                    card.style.transform = "translateX(0%)";
+                } else {
+                    card.style.transform = `translateX(${curr - shift}%)`;
+                }
             })
         }
     }
 
     return (
         <div className="carousel">
-            <button className="carousel-button left" onClick={moveCarousel}><img className="carousel-arrow" src={leftArrow} alt="left arrow" onClick={moveCarousel} /></button>
+            <button className="carousel-button left"><img className="carousel-arrow" src={leftArrow} alt="left arrow" onClick={moveCarousel} /></button>
             <div className="courses">
                 <CourseCard path="./images/what-is-web-development.png" name="Introduction to Web Development" org="freeCodeCamp" />
                 <CourseCard path="./images/making-your-first-web-app.png" name="Making your first web app" org="freeCodeCamp" />
@@ -49,7 +68,7 @@ function Carousel() {
                 <CourseCard path="./images/making-your-first-web-app.png" name="Making your first web app" org="freeCodeCamp" />
                 <CourseCard path="./images/what-is-web-development.png" name="Introduction to Web Development" org="freeCodeCamp" />
             </div>
-            <button className="carousel-button right" onClick={moveCarousel}><img className="carousel-arrow" src={rightArrow} alt="right arrow" onClick={moveCarousel} /></button>
+            <button className="carousel-button right"><img className="carousel-arrow" src={rightArrow} alt="right arrow" onClick={moveCarousel} /></button>
         </div>
     );
 }
