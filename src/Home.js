5db2083313fc3beab59cc0078d5106036c3e49f5
Matthew Sia <msia075@uottawa.ca>
2022-07-15 22:16:55 -0400

    change explore description

--- src/Home.js
+++ src/Home.js
@@ -36,7 +36,7 @@ function Home() {
             <Banner />
             <div className="explore-header">
                 <h1>Start Exploring</h1>
-                <p className="explore-description">Supercharge your learning by picking from our selection of over 5,000 courses and 150,000 videos</p>
+                <p className="explore-description">Browse some of our most popular courses this month</p>
             </div>
             <ExploreTab categories={homeCategories} />
             {carousels}
